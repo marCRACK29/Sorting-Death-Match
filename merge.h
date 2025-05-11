69cb6f2acfe1e8117c10cd74bd169e0c1c50a14b
marCRACK29 <mamartinez2023@udec.cl>
2025-05-11 16:17:07 -0400

    feat: Creación de algoritmo mergeSort.

---
+++ merge.h
@@ -0,0 +1,9 @@
+#ifndef MERGE_H
+#define MERGE_H
+#include <vector>
+using namespace std;
+
+void merge(vector<int>& arr, int left, int mid, int right);
+void mergeSort(vector<int>& arr, int left, int right);
+
+#endif  
