1e50bf865c4ac87e187cdd930abaa9910bb7c436
marCRACK29 <mamartinez2023@udec.cl>
2025-05-11 20:34:24 -0400

    refactor: cambios en ciclo while
    condicional if-else en ciclo while de ordenamiento en función merge se cambia por un operador ternario para hacerlo un poco más eficiente.

--- merge.cpp
+++ merge.cpp
@@ -8,34 +8,30 @@
 #include "merge.h"
 
 void merge(vector<int>& arr, int left, int mid, int right){
+    //Tamaño de los subarreglos
     int n1 = mid - left + 1; 
     int n2 = right - mid; 
-
     vector<int> L(n1), R(n2);
-
+    //Rellenar los subarreglos L y R
+    //con los elementos del arreglo original
     for (int i=0;i<n1;i++) L[i] = arr[left+i];
     for (int j=0;j<n2;j++) R[j] = arr[mid+1+j];
 
+    //Índices iniciales de los subarreglos
     int i = 0, j = 0; 
     int k = left; 
 
-    while (i<n1 && j<n2){
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
+    //Realiza el ordenamiento de los subarreglos
+    while (i<n1 && j<n2)
+        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+    
+    //Copiar los elementos restantes de los
+    //subarreglos L y R si los hay
     while (i<n1){
         arr[k] = L[i];
-        i++;
+        i++;    
         k++;
     }
-
     while (j<n2){
         arr[k] = R[j];
         j++;
@@ -44,8 +40,12 @@ void merge(vector<int>& arr, int left, int mid, int right){
 }
 
 void mergeSort(vector<int>& arr, int left, int right){
+    //Caso base
     if (left >= right) return; 
 
+    //Dividir el arreglo en dos mitades recursivamente.
+    //Luego, combinar los subarreglos ordenados
+    //en un solo arreglo ordenado.
     int mid = left + (right - left) / 2; 
     mergeSort(arr, left, mid);
     mergeSort(arr, mid+1, right);
