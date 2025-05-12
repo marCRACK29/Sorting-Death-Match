a03cdafac3be99ba1b35112b4179581570189dc6
marCRACK29 <mamartinez2023@udec.cl>
2025-05-11 21:17:22 -0400

    docs: Añade documentacion de código fuente en heap.cpp.

--- heap.cpp
+++ heap.cpp
@@ -8,12 +8,14 @@
 #include "heap.h"
 
 void heapify(vector<int>& arr, int n, int i){
-    int l = 2*i + 1; // hijo izquierdo
-    int r = 2*i + 2; // hijo derecho
+    int l = 2*i + 1; // Hijo izquierdo
+    int r = 2*i + 2; // Hijo derecho
     int largest = i;
 
     if (l<n && arr[l]>arr[largest]) largest = l;
     if (r<n && arr[r]>arr[largest]) largest = r; 
+    // Si el nodo padre es menor que el hijo
+    // entonces se intercambian los valores
     if (largest != i){
         swap(arr[i], arr[largest]); 
         heapify(arr, n, largest);
@@ -23,10 +25,12 @@ void heapify(vector<int>& arr, int n, int i){
 void heapSort(vector<int>& arr, int n){
     int start = (n/2)-1; // El último nodo que puede tener hijos esta en ese índice
     
+    // Construcción del heap
     for (int i=start;i>=0;i--)
         heapify(arr, n, i);
     
-    // Reordenamiento 
+    // Extracción de los elementos del heap
+    // y colocación en el arreglo
     for (int i=n-1;i>0;i--){ 
         swap(arr[0], arr[i]);
         heapify(arr,i,0);
