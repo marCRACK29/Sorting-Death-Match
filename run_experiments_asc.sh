ce4d419098b1251c0bd02a1a2254c67e4cfb8451
castle <gacasillo2023@udec.cl>
2025-05-28 11:06:17 -0400

    Finalizando archivos para correr experimentos con arreglos ascendentes y descendentes

---
+++ run_experiments_asc.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Imprimir una cabecera para el archivo CSV
+echo "ejecutable;numElementos;tiempo"
+
+# Repetir experimentos 30 veces
+for ((c=1; c<=30; c++))
+do
+    # Ejecutar el programa con diferentes tamaños de entrada
+    for n in 100000 200000 300000 400000 500000 
+    do
+        ./mergesort $n ascendente
+    done
+
+    for n in 100000 200000 300000 400000 500000
+    do 
+        ./heapsort $n ascendente
+    done
+
+    for n in 100000 200000 300000 400000 500000
+    do 
+        ./quicksort $n ascendente
+    done
+
+    for n in 100000 200000 300000 400000 500000
+    do 
+       ./insertionsort $n ascendente
+    done
+    
+    for n in 100000 200000 300000 400000 500000
+    do 
+        ./sort $n ascendente
+    done
+
+    for n in 100000 200000 300000 400000 500000
+    do 
+        ./timsort $n ascendente
+    done
+    
+done
