7db964086fb58b7886f6db9d1ccd04f32941dd11
marCRACK29 <mamartinez2023@udec.cl>
2025-06-07 15:14:00 -0400

    Ordenando repositorio

--- print_array.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "print_array.h"
-
-void printArray(vector<int>& arr){
-    for(int i=0;i<arr.size();i++)
-        cout << arr[i] <<" ";
-    cout << endl;
-}
--- print_array.h
+++
@@ -1,7 +0,0 @@
-#ifndef PRINT_ARRAY_H
-#define PRINT_ARRAY_H
-#include <vector>
-using namespace std;
-
-void printArray(vector<int>& arr);
-#endif
