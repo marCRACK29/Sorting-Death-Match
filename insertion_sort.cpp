2d703950264356a2bc4aae71bfea60e9188f37f2
marCRACK29 <mamartinez2023@udec.cl>
2025-06-07 15:02:02 -0400

    Mejora en insertionsort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,20 +2,31 @@
  * Autor principal: Gabriel Catillo Castillo (zMRGABOz)
  * Fecha: 13/05/2025
  * Descripción: Algoritmo insertion sort
- * Última modificación: 13/05/2025
+ * Última modificación: 07/06/2025  
  * Algoritmo inspirado de https://www.programiz.com/dsa/insertion-sort 
 *************************************/
 
 #include "insertion_sort.h"
 
 void insertionSort(vector<int>& arr, int left, int right) {
-    for (int i = left + 1; i <= right; i++) {
+    const int l = left;
+    const int r = right;
+
+    for (int i = l + 1; i <= r; ++i) {
         int temp = arr[i];
-        int j = i - 1;
-        while (j >= left && arr[j] > temp) {
-            arr[j + 1] = arr[j];
-            j--;
+        int j = i;
+
+        // Evita comparar si ya está ordenado
+        if (arr[j - 1] <= temp)
+            continue;
+
+        // Desplazar los elementos mayores
+        while (j > l && arr[j - 1] > temp) {
+            arr[j] = arr[j - 1];
+            --j;
         }
-        arr[j + 1] = temp;
+
+        // Solo escribir si hubo movimiento
+        arr[j] = temp;
     }
 }
