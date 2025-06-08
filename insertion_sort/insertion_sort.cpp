76e8e90809a8b0d7ba29dd1f321e9a05f92c4fa0
marCRACK29 <mamartinez2023@udec.cl>
2025-06-07 20:52:57 -0400

    agregando datasets a .gitignore (muy pesados).

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -3,7 +3,6 @@
  * Fecha: 13/05/2025
  * Descripción: Algoritmo insertion sort
  * Última modificación: 07/06/2025  
- * Algoritmo inspirado de https://www.programiz.com/dsa/insertion-sort 
 *************************************/
 
 #include "insertion_sort.h"
@@ -11,7 +10,7 @@
 void insertionSort(vector<int>& arr, int left, int right) {
     const int l = left;
     const int r = right;
-    /** 
+
     for (int i = l + 1; i <= r; ++i) {
         int temp = arr[i];
         int j = i;
@@ -29,19 +28,4 @@ void insertionSort(vector<int>& arr, int left, int right) {
         // Solo escribir si hubo movimiento
         arr[j] = temp;
     }
-    */
-
-    // Implementacion propia
-    for(int i = l + 1; i <= r; i++){ //El iterador comienza en 1 porque el primer elemento "ya está ordenado"
-        int act = arr[i]; //Guardamos el valor a insertar
-        int in = i;
-        int fin = i - 1;
-        while(fin >= 0 && act < arr[fin]){ //Mientras el elemento a insertar sea menor que el elemento en la posicion fin
-            arr[in] = arr[fin]; //Desplazamos el elemento hacia la derecha
-            in--;
-            fin--;
-        }
-        arr[in] = act; //Insercion del elemento en la posision correcta
-    }
-
 }
